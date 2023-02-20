51af4fe8cc73047b6f186fac32e5eef1d88bd984
Svetlana Koloskova <svetlanakoloskova99@gmail.com>
2023-02-20 16:00:51 +0700

    Task number 1

---
+++ script.js
@@ -0,0 +1,19 @@
+//Задание 1
+
+/*for (let counter = 1; counter <=2; counter++) {
+    console.log('Привет');
+}
+
+let counter = 1;
+
+while (counter <=2) {
+    alert('Привет');
+    counter++;
+}
+
+let counter = 1;
+
+do {
+    alert('Привет');
+    counter++;
+} while (counter <=2);*/
